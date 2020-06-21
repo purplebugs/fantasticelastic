83a2bfe6dc24f5869a0d964c3bbbf9553bcd4646
Anita Lipsky <anita@purplebugs.com>
2020-06-21 13:37:04 +0200

    Deploy website - based on 2f76904ab47e3e726dddc31be722064511d9165b

---
+++ 5dee59a4.328504d1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{114:function(l){l.exports=JSON.parse('{"allTagsPath":"/elasticdocs/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/elasticdocs/blog/tags/hello"}')}}]);
---
+++ 968a06af.109c2674.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{118:function(a){a.exports=JSON.parse('{"allTagsPath":"/elasticdocs/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/elasticdocs/blog/tags/hola"}')}}]);
