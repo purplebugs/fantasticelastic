5e8d286791990273657123b988ee5c99706bc4e6
Anita Lipsky <anita@purplebugs.com>
2020-11-30 16:55:08 +0100

    image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
     title: <>A Slow Journey</>,
-    imageUrl: 'img/car.png',
+    imageUrl: "img/car.png",
     description: (
       <>
-      Experts are not made overnight.  Enjoy the journey, one step at a time.
+        Experts are not made overnight. Enjoy the journey, one step at a time.
       </>
     ),
   },
   {
     title: <>Focus on Small Wins</>,
-    imageUrl: 'img/glasses.png',
+    imageUrl: "img/glasses.png",
     description: (
       <>
-        Make use of the small, simple use cases and code snippets with just enough in them to learning something new.
+        Make use of the small, simple use cases and code snippets with just
+        enough in them to learning something new.
       </>
     ),
   },
   {
     title: <>Take your time</>,
-    imageUrl: 'img/sleep.png',
+    imageUrl: "img/sleep.png",
     description: (
       <>
-        Learning something new is fun.  It is also hard.  After a session, take a break.
+        Learning something new is fun. It is also hard. After a session, take a
+        break.
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={classnames('col col--4', styles.feature)}>
+    <div className={classnames("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -53,22 +55,24 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={classnames("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                "button button--outline button--secondary button--lg",
+                styles.getStarted
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl("docs/")}
+            >
               Get Started
             </Link>
           </div>
